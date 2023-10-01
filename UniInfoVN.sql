3c2e95a62e79bb92d9b8584e3052c740610d4a55
minhngocgodric <119572309+minhngocgodric@users.noreply.github.com>
2023-10-01 09:02:53 +0700

    Add files via upload

---
+++ UniInfoVN.sql
@@ -0,0 +1,346 @@
+﻿GO
+INSERT INTO CACNGANH (mangang,tennganh) VALUES
+(
+'IT1',N'Khoa học máy tính'
+),
+(
+'IT2',N'Kỹ thuật máy tính'
+),
+(
+'IT-e10',N'Khoa học dữ liệu và Trí tuệ nhân tạo (DS&AI)'
+),
+(
+'IT-e15',N'An toàn không gian số'
+),
+(
+'IT-e6',N'CNTT Việt Nhật'
+),
+(
+'IT-e7',N'CNTT Global ICT'
+),
+(
+'IT-EP',N'CNTT Việt Pháp'
+);
+GO
+INSERT INTO PTXT (manganh,Ptxt) VALUES
+(
+'IT1',N'Xét tuyển'
+),
+(
+'IT1',N'THPTQG'
+),
+(
+'IT1',N'ĐGTD'
+),
+(
+'IT2',N'Xét tuyển'
+),
+(
+'IT2',N'THPTQG'
+),
+(
+'IT2',N'ĐGTD'
+),
+(
+'IT-e10',N'Xét tuyển'
+),
+(
+'IT-e10',N'THPTQG'
+),
+(
+'IT-e10',N'ĐGTD'
+),
+(
+'IT-e15',N'Xét tuyển'
+),
+(
+'IT-e15',N'THPTQG'
+),
+(
+'IT-e15',N'ĐGTD'
+),
+(
+'IT-e6',N'Xét tuyển'
+),
+(
+'IT-e6',N'THPTQG'
+),
+(
+'IT-e6',N'ĐGTD'
+),
+(
+'IT-e7',N'Xét tuyển'
+),
+(
+'IT-e7',N'THPTQG'
+),
+(
+'IT-e7',N'ĐGTD'
+),
+(
+'IT-EP',N'Xét tuyển'
+),
+(
+'IT-EP',N'THPTQG'
+),
+(
+'IT-EP',N'ĐGTD'
+);
+GO
+INSERT INTO DIEM (manganh,Ptxt,diem) VALUES
+(
+'IT1',N'Xét tuyển',100.7
+),
+(
+'IT1',N'THPTQG',0
+),
+(
+'IT1',N'ĐGTD',22.25
+),
+(
+'IT2',N'Xét tuyển',99.59
+),
+(
+'IT2',N'THPTQG',28.29
+),
+(
+'IT2',N'ĐGTD',21.19
+),
+( 
+'IT-e10',N'Xét tuyển',100.36
+),
+(
+'IT-e10',N'THPTQG',0
+),
+(
+'IT-e10',N'ĐGTD',22.68
+),
+(
+'IT-e15',N'Xét tuyển',75.41
+),
+(
+'IT-e15',N'THPTQG',0
+),
+(
+'IT-e15',N'ĐGTD',20.58
+),
+(
+'IT-e6',N'Xét tuyển',83.01
+),
+(
+'IT-e6',N'THPTQG',27.25
+),
+(
+'IT-e6',N'ĐGTD',18.39
+),
+(
+'IT-e7',N'Xét tuyển',75.63
+),
+(
+'IT-e7',N'THPTQG',0
+),
+(
+'IT-e7',N'ĐGTD',21.96
+),
+(
+'IT-EP',N'Xét tuyển',80.76
+),
+(
+'IT-EP',N'THPTQG',0
+),
+(
+'IT-EP',N'ĐGTD',16.26
+),
+(
+'IT1',N'Xét tuyển',90.17
+),
+(
+'IT1',N'THPTQG',29.42
+),
+(
+'IT1',N'ĐGTD',83.90
+),
+(
+'IT2',N'Xét tuyển',85.35
+),
+(
+'IT2',N'THPTQG',28.29
+),
+(
+'IT2',N'ĐGTD',79.22
+),
+(
+'IT-e10',N'Xét tuyển',98.42
+),
+(
+'IT-e10',N'THPTQG',28.80
+),
+(
+'IT-e10',N'ĐGTD',83.97
+),
+(
+'IT-e15',N'Xét tuyển',82.04
+),
+(
+'IT-e15',N'THPTQG',28.05
+),
+(
+'IT-e15',N'ĐGTD',76.61
+),
+(
+'IT-e6',N'Xét tuyển',80.25
+),
+(
+'IT-e6',N'THPTQG',27.64
+),
+(
+'IT-e6',N'ĐGTD',72.03
+),
+(
+'IT-e7',N'Xét tuyển',82
+),
+(
+'IT-e7',N'THPTQG',28.16
+),
+(
+'IT-e7',N'ĐGTD',79.12
+),
+(
+'IT-EP',N'Xét tuyển',80.01
+),
+(
+'IT-EP',N'THPTQG',27.32
+),
+(
+'IT-EP',N'ĐGTD',68.67
+);
+GO
+INSERT INTO nam(manganh,Ptxt,diem,nam) VALUES
+(
+'IT1',N'Xét tuyển',100.7,2022
+),
+(
+'IT1',N'THPTQG',0,2022
+),
+(
+'IT1',N'ĐGTD',22.25,2022
+),
+(
+'IT2',N'Xét tuyển',99.59,2022
+),
+(
+'IT2',N'THPTQG',28.29,2022
+),
+(
+'IT2',N'ĐGTD',21.19,2022
+),
+(
+'IT-e10',N'Xét tuyển',100.36,2022
+),
+(
+'IT-e10',N'THPTQG',0,2022
+),
+(
+'IT-e10',N'ĐGTD',22.68,2022
+),
+(
+'IT-e15',N'Xét tuyển',75.41,2022
+),
+(
+'IT-e15',N'THPTQG',0,2022
+),
+(
+'IT-e15',N'ĐGTD',20.58,2022
+),
+(
+'IT-e6',N'Xét tuyển',83.01,2022
+),
+(
+'IT-e6',N'THPTQG',27.25,2022
+),
+(
+'IT-e6',N'ĐGTD',18.39,2022
+),
+(
+'IT-e7',N'Xét tuyển',75.63,2022
+),
+(
+'IT-e7',N'THPTQG',0,2022
+),
+(
+'IT-e7',N'ĐGTD',21.96,2022
+),
+(
+'IT-EP',N'Xét tuyển',80.76,2022
+),
+(
+'IT-EP',N'THPTQG',0,2022
+),
+(
+'IT-EP',N'ĐGTD',16.26,2022
+),
+(
+'IT1',N'Xét tuyển',90.17,2023
+),
+(
+'IT1',N'THPTQG',29.42,2023
+),
+(
+'IT1',N'ĐGTD',83.90,2023
+),
+(
+'IT2',N'Xét tuyển',85.35,2023
+),
+(
+'IT2',N'THPTQG',28.29,2023
+),
+(
+'IT2',N'ĐGTD',79.22,2023
+),
+(
+'IT-e10',N'Xét tuyển',98.42,2023
+),
+(
+'IT-e10',N'THPTQG',28.80,2023
+),
+(
+'IT-e10',N'ĐGTD',83.97,2023
+),
+(
+'IT-e15',N'Xét tuyển',82.04,2023
+),
+(
+'IT-e15',N'THPTQG',28.05,2023
+),
+(
+'IT-e15',N'ĐGTD',76.61,2023
+),
+(
+'IT-e6',N'Xét tuyển',80.25,2023
+),
+(
+'IT-e6',N'THPTQG',27.64,2023
+),
+(
+'IT-e6',N'ĐGTD',72.03,2023
+),
+(
+'IT-e7',N'Xét tuyển',82,2023
+),
+(
+'IT-e7',N'THPTQG',28.16,2023
+),
+(
+'IT-e7',N'ĐGTD',79.12,2023
+),
+(
+'IT-EP',N'Xét tuyển',80.01,2023
+),
+(
+'IT-EP',N'THPTQG',27.32,2023
+),
+(
+'IT-EP',N'ĐGTD',68.67,2023
+);
+/*Cơ sở dữ liệu để người dùng lọc theo nhu cầu. VD: sinh viên nhập điểm, năm học -> những ngành học ở các trường có điểm chuẩn năm đó thấp hơn hoặc bằng điểm nhập vào
+Một trong những tính năng ưu điểm của sản phẩm sử dụng cơ sở dữ liệu SQL server*/
